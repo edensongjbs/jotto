624b46a3252e482698bb68d3b1dd769991779969
edensongjbs <edensongjbs@gmail.com>
2020-11-09 17:10:14 -0500

    congrats component tests passing

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default () => {
-    return (
-        <div></div>
-    )
+/**
+ * Function react component for congratulatory message
+ * @function
+ * @param {object} props - React props
+ * @return {JSX.Element} - Rendered component (or null if 'success' is falsy)
+ */
+
+export default (props) => {
+    if (props.success) {
+        return (
+        <div data-test="component-congrats">
+            <span data-test="congrats-message">
+                Congratulations!  You guessed the word!
+            </span>
+        </div>
+        )
+    }
+    else {
+        return (<div data-test="component-congrats"></div>)
+    }
 }
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -7,17 +7,24 @@ import {findByTestAttr} from '../test/testUtils'
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
 const setup = (props = {}) => {
-    return shallow(<Congrats {...props}/>)
+    return shallow(<Congrats {...props}/>) 
 }
 
 test('renders without error', () => {
-
+    const wrapper = setup()
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.length).toBe(1)
 })
 
 test('renders no text when "success" prop is false', () => {
-
+    const wrapper = setup({success: false})
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.text()).toBe('')
 })
 
 test('renders non-empty congrats message when "success" prop is true', () => {
-
+    const wrapper = setup({success: true})
+    const message = findByTestAttr(wrapper, 'congrats-message')
+    console.log(message)
+    expect(message.text().length).not.toBe(0)
 })
