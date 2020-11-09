45383e21eb2e583ddbc78dd2ed8188c865368639
edensongjbs <edensongjbs@gmail.com>
2020-11-09 18:15:07 -0500

    write tests for when no words are guessed

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -9,7 +9,7 @@ const defaultProps = {
 
 const setup = (props = {}) => {
     const setupProps = {...defaultProps, ...props}
-    return shallow(<GussedWords {...setupProps}/>)
+    return shallow(<GuessedWords {...setupProps}/>)
 }
 
 
@@ -18,7 +18,18 @@ test('does not throw warning with expected props', () => {
 })
 
 describe('if there are no words guessed', () => {
-
+    let wrapper
+    beforeEach(() => {
+        wrapper = setup({guessedWords:[]})
+    })
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words')
+        expect(component.length).toBe(1)
+    })
+    test('renders instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions')
+        expect(instructions.text().length).not.toBe(1)
+    })
 })
 
 describe('if there are words guessed', () => {
