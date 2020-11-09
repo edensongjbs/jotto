f6bff6197560e055f9f58d09216ff11800606366
edensongjbs <edensongjbs@gmail.com>
2020-11-09 18:19:02 -0500

    starting tests for guessed-words passing

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,8 +1,19 @@
 import PropTypes from 'prop-types'
 
-const GuessedWords = (props => {
-    return (<div></div>)
-})
+const GuessedWords = (props) => {
+    let contents
+    if (props.guessedWords.length === 0) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        )
+    }
+    return (
+        <div data-test="component-guessed-words">
+            { contents }
+        </div>)
+}
 
 GuessedWords.propTypes = {
     guessedWords: PropTypes.arrayOf(
