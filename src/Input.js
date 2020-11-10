c4270a2d218702fc24d8342586f1d6f9cf724043
edensongjbs <edensongjbs@gmail.com>
2020-11-10 14:41:49 -0500

    implement input component for when word is not yet guessed

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,26 @@ import { connect } from 'react-redux'
 
 class Input extends React.Component {
     render() {
-        return <div />
+        const contents = this.props.success
+        ? null
+        : (<form className="form-inline">
+            <input
+                type="text"
+                data-test="input-box" 
+                className="mb-2 mx-sm-3" 
+                placeholder="enter guess"/>
+            <button
+                data-test="submit-button"
+                type="submit"
+                className="btn btn-primary">
+                Submit
+            </button>
+        </form>)
+        return (
+            <div data-test="component-input">
+                {contents}
+            </div>
+        )
     }
 }
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -16,14 +16,22 @@ setup()
 
 describe('render', () => {
     describe('word has not been guessed', () => {
+        let wrapper
+        beforeEach(() => {
+            const initialState = {success: false}
+            wrapper = setup(initialState)
+        })
         test('renders component without error', () => {
-
+            const component = findByTestAttr(wrapper, 'component-input')
+            expect(component.length).toBe(1)
         })
         test('renders input box', () => {
-
+            const component = findByTestAttr(wrapper, 'input-box')
+            expect(component.length).toBe(1)
         })
         test('renders submit button', () => {
-
+            const component = findByTestAttr(wrapper, 'submit-button')
+            expect(component.length).toBe(1)
         })
     })
     describe('word has been guessed', () => {
