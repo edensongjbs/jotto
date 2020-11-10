99367de6c86cc8ffd852b33fee5d7afbfdd8c6a3
edensongjbs <edensongjbs@gmail.com>
2020-11-10 14:18:06 -0500

    design tests for input

---
+++ src/Input.test.js
@@ -0,0 +1,40 @@
+import { shallow } from 'enzyme'
+import { findByTestAttr, storeFactory } from '../test/testUtils'
+import { Provider } from 'react-redux'
+// import GuessedWords from './GuessedWords'
+import Input from './Input'
+// import store from './configureStore'
+
+const setup = (initialState={}) => {
+    const store = storeFactory(initialState)
+    const wrapper = shallow(<Input store={store}/>).dive().dive()
+    // console.log(wrapper.debug())
+    return wrapper
+}
+
+setup()
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        })
+        test('renders input box', () => {
+
+        })
+        test('renders submit button', () => {
+
+        })
+    })
+    describe('word has been guessed', () => {
+        test('renders component without error', () => {
+
+        })
+        test('does not render input box', () => {
+
+        })
+        test('does not render submit button', () => {
+
+        })
+    })
+})
