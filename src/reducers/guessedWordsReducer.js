9daa9d21b15a9d4ce1b91f7212f85b8f730aebc4
edensongjbs <edensongjbs@gmail.com>
2020-11-11 16:12:15 -0500

    merge guessedWordsReducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { actionTypes } from '../actions'
 
 export default (state =[], action) => {
@@ -8,3 +9,15 @@ export default (state =[], action) => {
             return state
     }
 }
+=======
+import actionTypes from '../actions/index'
+
+export default (state=[], action) => {
+    switch(action.type) {
+      case 'GUESS_WORD':
+        return [...state, action.payload];
+      default:
+        return state;
+    }
+  }
+>>>>>>> Stashed changes
